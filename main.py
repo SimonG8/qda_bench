bbdca2af7ff753b114d9f80a704b511d781f4de9
SimonG8 <117634710+SimonG8@users.noreply.github.com>
2026-01-18 18:16:23 +0100

    Empty Parameter in Main

--- main.py
+++ main.py
@@ -5,20 +5,18 @@ from quantum_bench.runner import run_benchmark
 
 if __name__ == "__main__":
     benchmarks = get_available_benchmark_names()
-    benchmarks.remove('grover')
-    benchmarks.remove('qwalk')
     hardware = get_available_device_names()
     # Startet die Benchmarking-Suite mit den definierten Parametern
     run_benchmark(
-        hardware_names=["ibm_falcon_27"], #"ionq_forte_36", "ionq_aria_25"
-        algo_names=["grover"],
-        qubit_ranges=[5],
-        benchmark_levels=["ALG"], #"alg", "indep", "nativegates", "mapped"
-        opt_levels=[3],
-        num_runs=1,
-        output_file="testing.csv",
-        run_visualisation=True,
-        run_verification=False,
-        run_plotter=False,
-        active_phases=[], # "rebase", "mapping", "optimization"
+        hardware_names=[], 
+        algo_names=[],
+        qubit_ranges=[],
+        benchmark_levels=[], #"ALG", "INDEP", "NATIVEGATES", "MAPPED"
+        opt_levels=[],
+        num_runs=,
+        output_file=,
+        run_visualisation=,
+        run_verification=,
+        run_plotter=,
+        #active_phases=[], # "REBASE", "MAPPING", "OPTIMIZATION"
     )
