c2404ce5b80251f8a940a757a53c5fb3a061c428
Terido <sterido8@gmail.com>
2026-01-19 20:21:25 +0100

    Final Benchmark

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     benchmarks = ["qft", "vqe_real_amp", "qaoa", "randomcircuit", "ghz"]
     hardware = ["ibm_eagle_127", "rigetti_ankaa_84", "ionq_forte_36"]
     qubits = [4, 5, 8, 10, 15, 16, 20, 25, 32, 36, 40, 50, 64, 75, 84, 100, 127]
-    run_mapping_benchmark(benchmarks, hardware, qubits, max_workers=4, max_queued_tasks=8)
-    run_compilation_benchmark(benchmarks, hardware, qubits, max_workers=4, max_queued_tasks=8)
+    run_mapping_benchmark(hardware, benchmarks, qubits, max_workers=4, max_queued_tasks=8)
+    run_compilation_benchmark(hardware, benchmarks, qubits, max_workers=4, max_queued_tasks=8)
 
     run_mapping_benchmark(["grover"], hardware, qubits, plot_path="mapping_resuls_grover.csv", max_workers=4,
                           max_queued_tasks=8)
